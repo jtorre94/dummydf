16a8ac1229bc12cdc7574338d3cc64b224a7a67f
jtorre94 <76944588+jtorre94@users.noreply.github.com>
2021-12-24 13:28:30 +0100

    Add long description README.md

--- setup.py
+++ setup.py
@@ -1,13 +1,32 @@
+import pathlib
 from setuptools import setup
 
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
 setup(
-    name='dummydf',
-    version='0.0.1',
+    name="dummydf",
+    version="0.0.3",
+    description="Generate a dummy dataframe for testing",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jtorre94/dummydf",
+    author="torre.preciado",
+    author_email="torre.preciado@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
     packages=['dummydf'],
     install_requires=[
         'numpy==1.21.5',
         'pandas==1.3.5',
         'PyYAML==6.0'
     ],
-    include_package_data=True,
 )
